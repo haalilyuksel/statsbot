3d24362765081a3f7bc9a6f3b0ea0b8274f614ba
haalilyuksel <76621628+haalilyuksel@users.noreply.github.com>
2021-02-12 12:02:43 +0300

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: '761598113386856482', // sunucu idsi
+  ownerRole:"768034089030647839", // sunucu sahiplerinin rolü
+  teyitsizRolleri:[], // kayıt olmayanlara verieln rol
+  jailRolu:"", // cezalılara verilen rol
+  enAltYetkiliRolu:[], // ilk yetkili permi
+  activity:"Youtube : Muhammed Demirel", // botun durumu
+  status:"idle",
+  invitelink:"", //davet linki
+  symbol:"", // tag
+  voicechannel:"" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
